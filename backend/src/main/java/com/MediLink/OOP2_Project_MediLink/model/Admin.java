8b5beec6ff686f354f6a4ee9799c1a49cf2f19d6
Michael Mbugua <michael.mbugua@strathmore.edu>
2025-06-01 01:13:35 +0300

    fix admin and doctor models

--- backend/src/main/java/com/MediLink/OOP2_Project_MediLink/model/Admin.java
+++ backend/src/main/java/com/MediLink/OOP2_Project_MediLink/model/Admin.java
@@ -5,39 +5,13 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "admin")
 public class Admin extends User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int doctorId;
-
-    @Column(nullable = false)
-    private String specialisation;
-
-    @Column(nullable = false)
-    private String hospitalName;
+    private int adminId;
 
     public Admin() {}
 
-    public Admin(String firstName, String lastName, String email, String phone, String password,
-                 String specialisation, String hospitalName) {
+    public Admin(String firstName, String lastName, String email, String phone, String password) {
         super(firstName, lastName, email, phone, password, null);
-        this.specialisation = specialisation;
-        this.hospitalName = hospitalName;
-    }
-
-    public void setSpecialisation(String specialisation) {
-        this.specialisation = specialisation;
-    }
-
-    public String getSpecialisation() {
-        return specialisation;
-    }
-
-    public void setHospitalName(String hospitalName) {
-        this.hospitalName = hospitalName;
-    }
-
-    public String getHospitalName() {
-        return hospitalName;
     }
 }
